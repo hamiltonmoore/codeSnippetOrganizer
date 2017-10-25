0eff8447f2763ad0e0b335ab383a726cfcfc3f1d
hamilton moore <hamlmoor@gmail.com>
2017-10-25 12:06:33 -0500

    add deployment mongouri

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const Snippet = require("./models/Snippet");
 
 const dbUrl = "mongodb://localhost:27017/codeSnippetOrganizer";
 mongoose.Promise = bluebird;
-let db = mongoose.connect(dbUrl);
+let db = mongoose.connect(MONGODB_URI || dbUrl);
 const port = process.env.PORT || 8000
 
 const app = express();
